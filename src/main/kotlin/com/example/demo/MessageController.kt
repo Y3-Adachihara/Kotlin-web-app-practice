4e7d6c72cfc61159132d34b9de064f94ee59b6e7
Your Name <san-j24001@sangi.jp>
2026-05-16 17:50:29 +0900

    import文でbind.bindとなっていたタイポを修正

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -1,7 +1,7 @@
 package com.example.demo
 
-import org.springframework.web.bind.bind.annotation.GetMapping
-import org.springframework.web.bind.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class MessageController {
