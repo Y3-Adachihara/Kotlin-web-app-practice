2570e8a0d082d27e296426716a2c8d5f26ce6d28
Your Name <san-j24001@sangi.jp>
2026-05-28 11:10:41 +0900

    ホーム画面を表示させるControllerとして変更

--- src/main/kotlin/com/example/demo/MessageController.kt
+++ src/main/kotlin/com/example/demo/MessageController.kt
@@ -1,13 +1,13 @@
 package com.example.demo
 
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class MessageController {
 
-    @GetMapping("/hello")
-    fun hello(): String {
-        return "Hello, Spring Boot!"
+    @GetMapping("/messages")
+    fun index(): String {
+        return "index"
     }
 }
